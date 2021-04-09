5f36324871e0158e54539c1b8d5adf32b413ff25
bing chen <bingchen2016@gmail.com>
2021-04-08 22:40:22 -0400

    finish json server and routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = ({ title, onAdd, showAdd }) => {
@@ -6,10 +7,12 @@ const Header = ({ title, onAdd, showAdd }) => {
   //  console.log('Click')
   //}
 
+  const location = useLocation()
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAdd? 'red' : 'green'} text={showAdd ? 'close' : 'Add'} onClick={onAdd} />
+      {location.pathname === '/' && <Button color={showAdd? 'red' : 'green'} text={showAdd ? 'close' : 'Add'} onClick={onAdd} />}
     </header>
   )
 }
